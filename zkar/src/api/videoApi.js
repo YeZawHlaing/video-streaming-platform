fa05fd55b6861f6327685acd322b9cd533081d11
yezawhlaing <yezawhlaing0@gmail.com>
2026-05-04 03:39:02 +0630

    url nginx

--- zkar/src/api/videoApi.js
+++ zkar/src/api/videoApi.js
@@ -1,7 +1,7 @@
 // src/api/videoApi.js
 import axios from "axios";
 
-const BASE_URL = "http://13.232.134.112:8080";
+const BASE_URL = "http://13.232.134.112";
 // const BASE_URL = "http://localhost:8000";
 // const BASE_URL = "https://zkar.onrender.com";
 
