f4ed8afafaab39c3388f57848defab65b471b2e4
yezawhlaing <yezawhlaing0@gmail.com>
2026-05-04 03:07:46 +0630

    url for local

--- zkar/src/api/videoApi.js
+++ zkar/src/api/videoApi.js
@@ -2,8 +2,8 @@
 import axios from "axios";
 
 // const BASE_URL = "http://3.111.149.234:8080";
-// const BASE_URL = "http://localhost:8080";
-const BASE_URL = "https://zkar.onrender.com";
+const BASE_URL = "http://localhost:8000";
+// const BASE_URL = "https://zkar.onrender.com";
 
 
 
