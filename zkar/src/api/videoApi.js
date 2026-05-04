73844614b5b05ffab222e385813355ca1a4c4763
yezawhlaing <yezawhlaing0@gmail.com>
2026-05-04 07:23:04 +0630

    domain

--- zkar/src/api/videoApi.js
+++ zkar/src/api/videoApi.js
@@ -1,9 +1,10 @@
 // src/api/videoApi.js
 import axios from "axios";
 
-const BASE_URL = "http://13.232.134.112";
+// const BASE_URL = "http://13.232.134.112";
 // const BASE_URL = "http://localhost:8000";
 // const BASE_URL = "https://zkar.onrender.com";
+const BASE_URL = "https://api.allkur.uk";
 
 
 
